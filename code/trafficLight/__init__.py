0145c17ca31359af0f85e04ccb9ac4497d84b5e0
James Dewes <james@jamesdewes.com>
2016-07-31 23:37:07 +0100

    Updated class references
    I changed the class reference so that it works, updated the change
    method to turn off the other lights and added a method to get the
    attributes so I could handle  them more simply

--- code/trafficLight/__init__.py
+++ code/trafficLight/__init__.py
@@ -1,15 +1,27 @@
-'''
+"""
 Keep track of the state of the light entity
-'''
+"""
 import light
 
 
 class TrafficLight:
+    colours = ["red", "yellow", "green"]
+
     def __init__(self):
-        self.red = light()
-        self.yellow = light()
-        self.green = light()
+        self.red = light.Light()
+        self.yellow = light.Light()
+        self.green = light.Light()
+
+    def __getitem__(self, item):
+        try:
+            return self.__dict__[item]
+        except KeyError:
+            return False
 
     def change(self, colour):
-        # type: (object) -> object colour =''):
-        self[colour].state != self[colour].state
+        # type: (object, string):
+        for c in self.colours:
+            if c == colour:
+                self[c].on()
+            else:
+                self[c].off()
